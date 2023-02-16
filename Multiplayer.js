8dfa9d4989e782da2e21c86e93b5312566049c9c
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-16 23:50:33 +0000

    test

--- Multiplayer.js
+++ Multiplayer.js
@@ -1,3 +1,12 @@
-const socket = io("https://invtoad.github.io/", {
-    reconnection: false,
-  });
+const socket = io("https://invtoadserver.github.io/", {
+  reconnection: false,
+});
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
