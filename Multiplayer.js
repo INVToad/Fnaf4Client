8b6408b4b966547c9aea72474404fec99f99cfa7
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-17 02:25:07 +0000

    lose server command

--- Multiplayer.js
+++ Multiplayer.js
@@ -9,7 +9,3 @@ const socket = io("https://invtoad.github.io/INVToadServer.github.io/", {
 socket.on('Connection', function() {
   consoel.log('user connected')
 });
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
