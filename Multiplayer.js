be4a7232887c9c1c0cf6aaee1aa370429cd8b094
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-19 23:30:04 +0000

    Join chat button

--- Multiplayer.js
+++ Multiplayer.js
@@ -2,18 +2,28 @@ const socket = io("https://FnafServer.jarethcochrane.repl.co", {
   reconnection: false,
 });
 
+var Button = document.getElementById("JoinChat")
+var JoinedChat = false
 
-socket.on('connect', function() {
-  socket.emit("connection", 'User Connected')
-});
-socket.on('disconnect', function() {
-  socket.emit("disconnection", 'User Diconnected')
-});
+if (JoinedChat) {
+  socket.on('connect', function() {
+    socket.emit("connection", 'User Connected')
+  });
+  socket.on('disconnect', function() {
+    socket.emit("disconnection", 'User Diconnected')
+  });
 
-socket.on('connected', function() {
-  console.log("User Connected")
-})
+  socket.on('connected', function() {
+    console.log("User Connected")
+  })
 
-socket.on('disconnected', function() {
-  console.log("User Disconnected")
-})
+  socket.on('disconnected', function() {
+    console.log("User Disconnected")
+  })
+}
+
+function ButtonControl() {
+  JoinedChat = true
+}
+
+Button.onclick = ButtonControl
