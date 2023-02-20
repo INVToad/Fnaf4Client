71545e25c250c16a366eb1e205ba0deda15998ae
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-20 00:03:58 +0000

    removed Button

--- Multiplayer.js
+++ Multiplayer.js
@@ -2,24 +2,16 @@ const socket = io("https://FnafServer.jarethcochrane.repl.co", {
   reconnection: false,
 });
 
-var button = document.getElementById("ButtonJoin")
-var Joined = false
+socket.on('connect', function() {
+  socket.emit("connection", 'User Connected')
+});
+socket.on('disconnect', function() {
+  socket.emit("disconnection", 'User Diconnected')
+});
+socket.on('connected', function() {
+  console.log("User Connected")
+})
 
-function Buttoon() {
-  socket.on('connect', function() {
-    socket.emit("connection", 'User Connected')
-  });
-  socket.on('disconnect', function() {
-    socket.emit("disconnection", 'User Diconnected')
-  });
-
-  socket.on('connected', function() {
-    console.log("User Connected")
-  })
-
-  socket.on('disconnected', function() {
-    console.log("User Disconnected")
-  })
-}
-
-button.onclick = Buttoon
+socket.on('disconnected', function() {
+  console.log("User Disconnected")
+})
